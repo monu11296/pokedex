223c90f46bca3a689af4820a1bbcef6655464c27
Manoranjan <i.manoranjan.choudhary@redcarpetup.com>
2019-12-16 11:56:02 +0530

    enabled cors

--- backend/python/main.py
+++ backend/python/main.py
@@ -5,6 +5,8 @@
 from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
+from starlette.middleware.cors import CORSMiddleware
+
 from sqlalchemy.orm import Session
 
 from utils import get_pokemons
@@ -18,6 +20,19 @@ Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency
 def get_db():
